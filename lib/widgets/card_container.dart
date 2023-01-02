7ce7b398a66f53058a0144227a667aee361df57a
PuaghDev <pablofeijo181@gmail.com>
2023-01-02 11:17:00 -0300

    primer commit

---
+++ lib/widgets/card_container.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class CardContainer extends StatelessWidget {
+  const CardContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      color: Colors.red,
+      height: 300,
+    );
+  }
+}
